93a29ae55d930b8e3bdf1b66e5c30c6d52c29c93
yashguptaFE <yash.gupta@smallcase.com>
2023-05-10 17:59:28 +0530

    style: implements lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./App.css";
+
+const CakeDirectory = lazy(() => import("./pages/CakeDirectory"));
+const AdminControl = lazy(() => import("./pages/AdminControl"));
+const CakePage = lazy(() => import("./pages/CakePage"));
 
 function App() {
   return (
     <div className="App">
       <Layout />
+      <Suspense fallback={<span>Loading...</span>}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<CakeDirectory />} />
+            <Route path="/admin" element={<AdminControl />} />
+            <Route path="/:cakeId" element={<CakePage />} />
+          </Routes>
+        </Router>
+      </Suspense>
     </div>
   );
 }
